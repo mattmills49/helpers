d8b755020e538db611473c97c63522db28bab656
mattmills49 <mattmills49@gmail.com>
2016-07-08 17:03:02 -0400

    Added sample_group function

---
+++ R/sample_group.R
@@ -0,0 +1,17 @@
+#' sample from groups of variables
+#'
+#' \code{sample_group} allows you to sample observations from groupings of
+#' variables. This is useful for when you have multilevel data and would like
+#' to sample from multiple levels at once. This makes exploratory plotting a
+#' little easier.
+#'
+#' @param df the data.frame you are sampling from
+#' @param ... the variables you would like to sample from
+#' @param n the number of samples to return
+#' @return a data frame containing the samples groupings
+#' @examples
+#'
+
+sample_group <- function(df, ..., n){
+  return(suppressMessages(dplyr::inner_join(df, dplyr::sample_n(dplyr::distinct(dplyr::select(df, ...)), size = n))))
+}
